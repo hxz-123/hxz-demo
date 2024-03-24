dd80bed8a7a12ddb8bffe93bec1a48d0793f8348
DELL <2423059092@qq.com>
2024-03-24 13:33:17 +0800

    ssl认证

---
+++ src/main/java/com/abc/utils/HttpsConfig.java
@@ -0,0 +1,38 @@
+package com.abc.utils;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * https配置，将http请求全部转发到https
+ * @author Jacob
+ */
+@Configuration
+public class HttpsConfig {
+
+    @Value("${custom.http-port: 8080}")
+    private Integer httpPort;
+
+    @Bean
+    public TomcatServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(httpConnector());
+        return tomcat;
+    }
+
+    @Bean
+    public Connector httpConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(httpPort);
+        return connector;
+    }
+
+}
+
+
